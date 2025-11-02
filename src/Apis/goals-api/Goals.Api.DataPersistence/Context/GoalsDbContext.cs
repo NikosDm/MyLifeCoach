fd85ed8125affdabf00ed7d4127dff50fc8c56a8
Nikos Thomadakis <Nikos Thomadakis>
2025-11-03 01:23:58 +0200

    Remove deprecated sets

--- src/Apis/goals-api/Goals.Api.DataPersistence/Context/GoalsDbContext.cs
+++ src/Apis/goals-api/Goals.Api.DataPersistence/Context/GoalsDbContext.cs
@@ -1,13 +1,9 @@
-using Goals.Api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Goals.Api.DataPersistence.Context;
 
 public sealed class GoalsDbContext(DbContextOptions<GoalsDbContext> options) : DbContext(options)
 {
-    public DbSet<Goal> Goals { get; set; }
-    public DbSet<GoalType> GoalTypes { get; set; }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
