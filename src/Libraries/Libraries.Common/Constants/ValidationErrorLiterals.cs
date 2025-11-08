9ceb6aae1eb40f8b2200a44828940d6509c05653
Nikos Thomadakis <Nikos Thomadakis>
2025-11-08 23:58:10 +0200

    Add new literals

--- src/Libraries/Libraries.Common/Constants/ValidationErrorLiterals.cs
+++ src/Libraries/Libraries.Common/Constants/ValidationErrorLiterals.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Libraries.Common.Constants;
 
 public static class ValidationErrorLiterals
@@ -8,4 +6,7 @@ public static class ValidationErrorLiterals
     public const string ParameterExceedLimit = "{0} should not exceed {1} characters";
     public const string NegativeNumericParameterValue = "{0} cannot have a negative value";
     public const string InvalidGivenPeriod = "EndDate must be later than StartDate.";
+    public const string BetweenParameter = "{0} must be between {1} and {2}";
+    public const string InvalidEmailAddressFormat = "Invalid email address format";
+    public const string InvalidParameter = "{0} is invalid";
 }
