582ae9eb364e3c9e5016b94f086c34355e4c2436
Nikos Thomadakis <niktom726@gmail.com>
2025-11-23 19:30:00 +0200

    Remove deprecated check

--- src/Libraries/Libraries.Common/Entities/BaseEntity.cs
+++ src/Libraries/Libraries.Common/Entities/BaseEntity.cs
@@ -1,11 +1,12 @@
 using System;
+
 using Libraries.Common.Exceptions;
 
 namespace Libraries.Common.Entities;
 
 public abstract class BaseEntity : IEntity<Guid>
 {
-    public Guid Id { get; private set; }    
+    public Guid Id { get; private set; }
     public Guid CreatedBy { get; set; }
     public Guid LastUpdatedBy { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
@@ -13,7 +14,6 @@ public abstract class BaseEntity : IEntity<Guid>
 
     public void SetEntityId(Guid value)
     {
-        ArgumentNullException.ThrowIfNull(value);
         if (value == Guid.Empty)
         {
             throw new DomainException("Entity Id value cannot be empty");
