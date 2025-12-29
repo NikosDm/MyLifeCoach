3f0136ab43763a39437652833811083b00c59d0b
Nikos Thomadakis <niktom726@gmail.com>
2025-12-29 08:38:12 +0200

    Extend identity models

--- src/Apis/identity-server/IdentityServer.DataAccess/Entities/ApplicationRole.cs
+++ src/Apis/identity-server/IdentityServer.DataAccess/Entities/ApplicationRole.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace IdentityServer.DataAccess.Entities;
@@ -7,7 +6,7 @@ public class ApplicationRole : IdentityRole
 {
     public ApplicationRole()
     { }
-    
+
     public ApplicationRole(string roleName)
         : base(roleName)
     { }
--- src/Apis/identity-server/IdentityServer.DataAccess/Entities/ApplicationUser.cs
+++ src/Apis/identity-server/IdentityServer.DataAccess/Entities/ApplicationUser.cs
@@ -3,4 +3,8 @@ using Microsoft.AspNetCore.Identity;
 namespace IdentityServer.DataAccess.Entities;
 
 public class ApplicationUser : IdentityUser
-{ }
+{
+    public string FullName { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsPendingVerification { get; set; }
+}
