5a3c81ab0bde87c51f63b756f005a3aab7039cfa
Nikos Thomadakis <niktom726@gmail.com>
2026-02-23 05:06:25 +0200

    Remove generation of ids on database

---
+++ src/Apis/goals-api/Goals.Api.DataPersistence/Migrations/20260221173039_RemoveGenerationOfIdOnDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Goals.Api.DataPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGenerationOfIdOnDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
