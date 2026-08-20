8ab9c31ec7aa006ccd2f7d9aa760a3042dc9d194
Nikos Thomadakis <niktom726@gmail.com>
2026-08-21 02:58:35 +0300

    Add DeactivationDate for Identity user model

--- src/Apis/identity-server/IdentityServer.DataAccess/Entities/ApplicationUser.cs
+++ src/Apis/identity-server/IdentityServer.DataAccess/Entities/ApplicationUser.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Microsoft.AspNetCore.Identity;
 
 namespace IdentityServer.DataAccess.Entities;
@@ -6,4 +8,5 @@ public class ApplicationUser : IdentityUser
 {
     public string FullName { get; set; }
     public bool IsActive { get; set; }
+    public DateTimeOffset? DeactivationDate { get; set; }
 }
---
+++ src/Apis/identity-server/IdentityServer.DataAccess/Migrations/20260502003244_AddDeactivationDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityServer.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeactivationDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeactivationDate",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeactivationDate",
+                table: "AspNetUsers");
+        }
+    }
+}
