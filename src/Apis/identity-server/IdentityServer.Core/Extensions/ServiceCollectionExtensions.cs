7fbb7fc13d3b4141c8a7017385f4816d906213a8
Nikos Thomadakis <niktom726@gmail.com>
2026-08-21 03:01:22 +0300

    Update service registrations

--- src/Apis/identity-server/IdentityServer.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/Apis/identity-server/IdentityServer.Core/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using IdentityServer.Core.Abstractions;
 using IdentityServer.Core.Services;
+using IdentityServer.Core.Subscribers;
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,7 +11,24 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddIdentityServerCore(this IServiceCollection services)
     {
         services
+            .AddServices()
+            .AddSubscribers();
+
+        return services;
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services
             .AddScoped<IAccountService, AccountService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddSubscribers(this IServiceCollection services)
+    {
+        services
+            .AddScoped<UserActivationChangedSubscriber>();
 
         return services;
     }
