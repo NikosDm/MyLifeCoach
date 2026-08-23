222bd9020beec8543c8836c16593c94cb2793932
Nikos Thomadakis <niktom726@gmail.com>
2026-08-23 06:15:42 +0300

    Remove deprecated hook and axios client

--- src/Clients/MyLifeCoachWeb/src/auth/index.ts
+++ src/Clients/MyLifeCoachWeb/src/auth/index.ts
@@ -3,6 +3,5 @@ export { default as LoginButton } from "../components/auth/LoginButton";
 export { default as LogoutButton } from "../components/auth/LogoutButton";
 export { default as PrivateRoute } from "../components/auth/PrivateRoute";
 export { default as SignInCallback } from "../components/auth/SignInCallback";
-export { default as createApiClient } from "./apiClient";
 export { oidcConfig, API_URLS } from "./config";
 export type { AuthContextType, ApiErrorResponse } from "./types";
