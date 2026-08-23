1ad1426c31d8de688be877f9eef7f11a26b68c8f
Nikos Thomadakis <niktom726@gmail.com>
2026-08-23 06:07:54 +0300

    Add migration for DeactivationDate

---
+++ src/Apis/profiles-api/Profiles.Api.DataPersistence/Migrations/20260502003031_AddDeactivationDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Profiles.Api.DataPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeactivationDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeactivationDate",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeactivationDate",
+                table: "User");
+        }
+    }
+}
