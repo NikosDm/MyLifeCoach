5e2aaa72746606e5192917540576dfe104af8882
Nikos Thomadakis <niktom726@gmail.com>
2026-08-23 06:16:54 +0300

    Remove deprecated root page

--- src/Clients/MyLifeCoachWeb/src/pages/root/RootPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, Typography } from "@mui/material";
-
-export default function RootPage() {
-  return (
-    <Container maxWidth="xl" sx={{ mt: 16 }}>
-      <Typography variant="h4">Welcome to My Life Coach!</Typography>
-    </Container>
-  );
-}
