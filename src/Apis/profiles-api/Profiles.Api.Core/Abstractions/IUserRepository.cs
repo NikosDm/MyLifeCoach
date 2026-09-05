414b06d1e0930b697c85cc3e548f764ac760e0e2
Nikos Thomadakis <niktom726@gmail.com>
2026-09-05 23:15:20 +0300

    Extend IUserRepository to retrieve users with pagination

--- src/Apis/profiles-api/Profiles.Api.Core/Abstractions/IUserRepository.cs
+++ src/Apis/profiles-api/Profiles.Api.Core/Abstractions/IUserRepository.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Libraries.Common.Models;
 
 using Profiles.Api.Domain.Models;
 
@@ -9,8 +13,14 @@ namespace Profiles.Api.Core.Abstractions;
 
 public interface IUserRepository
 {
+    Task<PaginationResult<UserListItemResponse>> GetPaginatedAsync<UserListItemResponse>(
+        int pageNumber,
+        int pageSize,
+        Expression<Func<User, UserListItemResponse>> selectOptions,
+        Expression<Func<User, bool>> whereOptions = null,
+        Func<IQueryable<User>, IOrderedQueryable<User>> orderBy = null,
+        CancellationToken token = default);
     Task<IReadOnlyList<User>> GetAsync(CancellationToken token = default);
     Task<User> GetByIdAsync(Guid id, CancellationToken token = default);
     Task<User> CreateAsync(User user, bool saveChanges = true, CancellationToken token = default);
-    Task<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken token = default);
 }
